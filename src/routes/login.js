6d5e91a507411ebf6970ed6aa31b1173f16e674d
Moorcroft, Neil <Neil.Moorcroft@zuhlke.com>
2015-12-10 14:21:22 +0000

    bcrypt passwords

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,7 +4,9 @@ var _ = require('lodash');
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var fs = require('fs');
+var bcrypt = require('bcrypt');
 var nconf = require('nconf');
+var bcrypt = require('bcrypt');
 var router = express.Router();
 
 var users = JSON.parse(fs.readFileSync(nconf.get('users')));
@@ -14,7 +16,7 @@ router.post('/login', function (req, res) {
     var credentials = req.body;
     var user = users[credentials.username];
 
-    if (!user || user.password != credentials.password) {
+    if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
         res.status(401).send('invalid username or password');
 
     } else {
